690678f42ae8270d6c3f327238fadf73b0b549f4
sebspetersen <sebspetersen@gmail.com>
2024-07-12 10:28:38 +0200

    Create App.js

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import ColorBlindnessFilterApp from './ColorBlindnessFilterApp';
+
+function App() {
+  return (
+    <div className="App">
+      <ColorBlindnessFilterApp />
+    </div>
+  );
+}
+
+export default App;
